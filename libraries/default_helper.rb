0b246c942be30fea767fe34a7d6ce422825461cd
Victor Delgado <vdel26@gmail.com>
2015-10-27 22:23:10 +0100

    addd header info to library

--- libraries/default_helper.rb
+++ libraries/default_helper.rb
@@ -1,3 +1,12 @@
+#
+# Cookbook Name:: chef-3scale
+# Recipe:: default
+#
+# Copyright (C) 2015 3scale Inc. (https://3scale.net)
+#
+# All rights reserved - Do Not Redistribute
+#
+
 class Chef::Recipe::Helpers
   def self.is_config?(filename)
     ['.lua', '.conf'].include?(File.extname(filename))
