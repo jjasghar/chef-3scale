80dcbee7aa96c0173a809a7e73f0bfe0a0c2fa07
Victor Delgado <vdel26@gmail.com>
2015-10-28 13:04:11 +0100

    integration tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,7 +1,48 @@
 require 'spec_helper'
 
 describe 'chef-3scale::default' do
-  it 'should work ok' do
-    skip 'replace this with meaningful tests'
+
+  ###############
+  # nginx service
+  ###############
+
+  describe service('nginx') do
+    it { should be_enabled }
+    it { should be_running }
   end
+
+  describe command('/usr/sbin/nginx -V') do
+    its(:stderr) { should contain('nginx version: openresty') }
+  end
+
+  describe command('curl localhost:80') do
+    its(:stdout) { should contain('Welcome to nginx!') }
+  end
+
+  ###########
+  # log files
+  ###########
+
+  describe file('/var/log/nginx/error.log') do
+    it { should be_file }
+  end
+
+  ##############
+  # config files
+  ##############
+
+  describe file('/etc/nginx/nginx.conf') do
+    it { should be_file }
+    it { should be_symlink }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+  end
+
+  describe file('/etc/nginx/nginx.sample.lua') do
+    it { should be_file }
+    it { should be_symlink }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+  end
+
 end
