47bdf2080569624ee08a4088224a213354cca846
Victor Delgado <vdel26@gmail.com>
2015-11-09 18:40:14 +0100

    bump to 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support@3scale.net'
 license          'MIT'
 description      'Install and configures the 3scale API gateway'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 supports 'ubuntu'
 supports 'centos'
