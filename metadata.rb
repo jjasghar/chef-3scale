031618d55159b79f7bf79f06752d8244271cbfdb
Maria Pilar Guerra Arias <mpguerra@users.noreply.github.com>
2016-10-31 10:25:03 +0100

    Update metadata.rb
    Fix jenkins, add issues_url and source_url as per foodcritic FC064 and FC065

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license          'MIT'
 description      'Install and configures the 3scale API gateway'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.3.0'
+issues_url       'https://github.com/3scale/chef-3scale/issues'
+source_url       'https://github.com/3scale/chef-3scale'
 
 supports 'ubuntu'
 supports 'centos'
